5aa46ca499f554c04a7235aca851623aeee39256
Douglas Naphas <douglasnaphas@gmail.com>
2023-03-14 11:07:22 -0400

    Apply configs in order of precedence

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-import hcl2
+import hcl2, json
 
 def main():
-    with open('terraform/outputs.tf', 'r') as outputs:
-        outputs_dict = hcl2.load(outputs)
-        print(outputs_dict)
+    with open('terraform/variables.tf', 'r') as variables:
+        variables_dict = hcl2.load(variables)
+    print(json.dumps(variables_dict))
+    config_files = ['regional-configs.json', 'app-configs.json']
+    for config_file in config_files:
+        with open('configs/' + config_file, 'r') as c:
+            configs_dict = json.load(c)
+            print("configs, " + config_file + ":")
+            print(configs_dict)
+            for prop in configs_dict:
+                for idx,v in enumerate(variables_dict['variable']):
+                    if v.get(prop):
+                        variables_dict['variable'][idx][prop]['value'] = configs_dict[prop]
+    print(json.dumps(variables_dict))
 
 if __name__ == "__main__":
     main()
